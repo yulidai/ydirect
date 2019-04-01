8e19597600c762dd1dcbcbc174837111ca4dea39
leedy <yulidaiahx@163.com>
2019-04-01 20:20:54 +0800

    translate chinese to english via youdao api

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"net/http"
+	//"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	if len(os.Args) < 2 {
+		fmt.Println("missing parameters")
+		return
+	}
+
+	url := fmt.Sprintf("http://dict.youdao.com/w/%s", os.Args[1])
+	//fmt.Printf("查询单词: %s\n", url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+
+	doc.Find("#phrsListTab .trans-container > ul > li").Each(func(i int, s *goquery.Selection) {
+		fmt.Printf("%s\n", s.Text())
+	})
 }
